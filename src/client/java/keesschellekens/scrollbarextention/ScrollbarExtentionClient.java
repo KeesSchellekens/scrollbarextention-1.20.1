b83657e988e65b615d712dbcd8643dae8eb7679e
Kees  Schellekens <k.b.r.schellekens@student.tue.nl>
2023-11-01 22:41:57 +0100

    first commit

---
+++ src/client/java/keesschellekens/scrollbarextention/ScrollbarExtentionClient.java
@@ -0,0 +1,10 @@
+package keesschellekens.scrollbarextention;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScrollbarExtentionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
